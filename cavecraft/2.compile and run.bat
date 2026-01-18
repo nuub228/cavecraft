3a9ce82c3e577b62e659e35df8ee5139727abb32
nuub228 <54170775+nuub228@users.noreply.github.com>
2026-01-18 05:26:41 +0300

    Update 2.compile and run.bat

--- cavecraft/2.compile and run.bat
+++ cavecraft/2.compile and run.bat
@@ -1,7 +1,8 @@
 @echo off
 mkdir releasex64
-"gcc-15.2.0-gdb-16.3.90.20250511-binutils-2.45-mingw-w64-v13.0.0-ucrt\bin\g++.exe" main.cpp -I"sdl2\include" -L"sdl2\lib" -lSDL2_image -lmingw32 -lSDL2main -lSDL2 -lSDL2_ttf -o releasex64\program.exe
+g++ main.cpp -I"sdl2\include" -L"sdl2\lib" -lSDL2_image -lmingw32 -lSDL2main -lSDL2 -lSDL2_ttf -o releasex64\program.exe
 cd releasex64
 program.exe
 
 pause	
+
