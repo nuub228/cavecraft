814f6938231e4440cfcf2dd6fd6ddf5b22ec916c
nuub228 <54170775+nuub228@users.noreply.github.com>
2026-01-18 05:26:51 +0300

    Update 1.just_compile.bat

--- cavecraft/1.just_compile.bat
+++ cavecraft/1.just_compile.bat
@@ -1,8 +1,9 @@
 @echo off
 
 mkdir releasex64
-"g++" main.cpp -I"sdl2\include" -L"sdl2\lib" -lSDL2_image -lmingw32 -lSDL2main -lSDL2 -lSDL2_ttf -mwindows -o releasex64\program.exe
+g++ main.cpp -I"sdl2\include" -L"sdl2\lib" -lSDL2_image -lmingw32 -lSDL2main -lSDL2 -lSDL2_ttf -mwindows -o releasex64\program.exe
 
 
 pause	
+
 
